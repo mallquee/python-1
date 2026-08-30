f0c51662c9701a5f377c756b3941df9bcdfe9ec4
malala <fghiabcde363@gmail.com>
2026-08-30 19:17:59 +0700

    Tugas pertemuan 2 tipe-data

---
+++ tipe-data.py
@@ -0,0 +1,36 @@
+nama = "Mala Lailatul Arifah"
+print("Nama:", nama)
+print("- bertipe", type(nama))
+
+umur = 18
+print("Umur:", umur, "Tahun")
+print("- bertipe", type(umur))
+
+berat = 56.4
+print("Berat:", berat, "kg")
+print("- bertipe", type(berat))
+
+angka_str = "123"
+angka_int = int(angka_str)
+print("Angka Integer:", angka_int)
+
+angka_float = 45.67
+angka_int = int(angka_float)
+print("Angka Integer:", angka_int)
+
+angka_int = 89
+angka_float = float(angka_int)
+print("Angka Float:", angka_float)
+
+angka_int = 89
+angka_str = str(angka_int)
+print("Angka String:", angka_str)
+
+usia = int(input("Masukan Usia: ")) 
+print("data ",usia,",type =",type(usia))
+
+tinggi = float(input("Masukan Tinggi Badan: ")) 
+print("data ",tinggi,",type =",type(tinggi))
+
+nama = str(input("Masukan Nama: ")) 
+print("data ",nama,",type =",type(nama))
