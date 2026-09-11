e6bdc0cea84cb6a19437c27feebb42b4e20df726
malala <fghiabcde363@gmail.com>
2026-09-11 08:08:34 +0700

    Pertemuan 3

---
+++ operasi_aritmatika.py
@@ -0,0 +1,17 @@
+panjang = 12
+lebar = 5
+tinggi = 8
+
+luas = 2 * (panjang * lebar + panjang * tinggi + lebar * tinggi)
+volume = panjang * lebar * tinggi
+keliling = 4 * (panjang + lebar + tinggi)
+
+print('Luas =', luas)
+print('Volume =', volume)
+print('Keliling =', keliling)
+
+hasil = luas > 50 
+print(luas,'>',50,'=',hasil)
+
+hasil = volume == 480
+print(volume,'==',480,'=',hasil)
